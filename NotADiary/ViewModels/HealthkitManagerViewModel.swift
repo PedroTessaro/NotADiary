105582777188cc504a8bf6ba0a8f1fadea641474
Vinicius A. Marques <vini.amarques10@gmail.com>
2025-10-10 15:27:46 -0300

    feat: healthKit emotions integration

--- NotADiary/ViewModels/HealthkitManagerViewModel.swift
+++ NotADiary/ViewModels/HealthkitManagerViewModel.swift
@@ -7,19 +7,28 @@ class HealthManager{
     public static let shared = HealthManager()
     
     private var steps = 0
+    private var results: [HKStateOfMind] = []
     
-    // MARK: - Methods
+    // MARK: - Types
+    
+    private let moodType = HKObjectType.stateOfMindType()
+    private let stepsType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+    
+    // MARK: - Authorization
     
     func requestHealthAuthorization() async {
         
+        //tratar a falta de disponiblidade do healthkit
         guard HKHealthStore.isHealthDataAvailable() else { return }
         
         // Request authorization to read the user's step count from HealthKit
-        try? await HKHealthStore().requestAuthorization(toShare: [], read: [HKQuantityType(.stepCount)])
-        
+        //tratar a falta de autorizacao do usuario
+        try? await HKHealthStore().requestAuthorization(toShare: [moodType], read: [stepsType,moodType])
     }
     
-     func calculateSteps() async {
+    // MARK: - GettingSteps
+    
+    func calculateSteps() async {
         
         // To get the day's steps, start from midnight and end now
         let dateEnd = Date.now
@@ -53,15 +62,55 @@ class HealthManager{
         
     }
     
-    func getSteps() -> Int {
-        print(steps)
-        return steps
+    // MARK: - Writing Mood
+    
+    /// Create State of Mind sample for an event and emoji selection
+    
+    func createSample(eventAssociation: HKStateOfMind.Association, userLabel: HKStateOfMind.Label, userValence: Double, endDate: Date) -> HKStateOfMind {
+        let kind: HKStateOfMind.Kind = .momentaryEmotion
+        let valence: Double = userValence
+        let label = userLabel
+        let association = eventAssociation
+        return HKStateOfMind(date: endDate,
+                             kind: kind,
+                             valence: valence,
+                             labels: [label],
+                             associations: [association])
     }
     
-    func getEmotions() -> Int {
-        print(steps)
-        return steps
+    func save(sample: HKSample) async {
+        do {
+            try await HKHealthStore().save(sample)
+        }
+        catch {
+            // Handle error here.
+        }
     }
     
+    // MARK: - Reading Mood
+    
+    // Busca os moods registrados num intervalo (ex: últimos 7 dias)
+    
+    func fetchMoods() async -> [HKStateOfMind] {
+        
+        
+        let startDate = Calendar.current.date(byAdding: .day, value: -7, to: Date())!
+        let datePredicate = HKQuery.predicateForSamples(withStart: startDate, end: Date(), options: .strictStartDate)
+        
+        let stateOfMindPredicate = HKSamplePredicate.stateOfMind(datePredicate)
+        
+        let descriptor = HKSampleQueryDescriptor(predicates: [stateOfMindPredicate],
+                                                 sortDescriptors: [])
+        do {
+            // Launch the query and wait for the results.
+            results = try await descriptor.result(for: HKHealthStore())
+        } catch {
+            // Handle error here.
+        }
+        return results
+    }
+        func getSteps() -> Int {
+            print(steps)
+            return steps
+        }
 }
-
--- NotADiary/Views/ContentView.swift
+++ NotADiary/Views/ContentView.swift
@@ -8,7 +8,8 @@ struct ContentView: View {
     // MARK: - Properties
     
     @State private var steps = 0
-    
+    @State private var results: [HKStateOfMind] = []
+    @State private var relato: HKStateOfMind?
     
     
     // MARK: - Body
@@ -16,6 +17,11 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("You walked \(steps) steps today!")
+            ForEach(results, id: \.self) { result in
+                Text("Emotions: \(result)")
+            }
+            Text("\(relato ?? nil)")
+            
         }
         .task {
             await HealthManager.shared.requestHealthAuthorization()
@@ -25,6 +31,9 @@ struct ContentView: View {
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
                 steps = HealthManager.shared.getSteps()
             }
+            results = await HealthManager.shared.fetchMoods()
+            relato = HealthManager.shared.createSample(eventAssociation: HKStateOfMind.Association.family, userLabel: HKStateOfMind.Label.amazed, userValence: 1, endDate: Date())
+            await HealthManager.shared.save(sample: relato!)
         }
         
     }
