db7ace975bf70bb41534166fc406cc430aed39a0
Vinicius A. Marques <vini.amarques10@gmail.com>
2025-10-24 14:39:42 -0300

    fix: incorrect documentation

--- NotADiary/NotADiaryApp.swift
+++ NotADiary/NotADiaryApp.swift
@@ -12,7 +12,6 @@ struct NotADiaryApp: App {
     var body: some Scene {
         WindowGroup {
             EntryView()
-            //Talvez seja assim que você recebe e trata um arquivo
                 .onOpenURL { URL in
                     print(URL)
                 }
